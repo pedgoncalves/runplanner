555b9824b5371e7c8b6e3f01977fcc405bfae706
Pedro <pedgoncalves@gmail.com>
2015-08-11 17:31:16 -0300

    TopActivity

---
+++ RunPlanner/src/br/com/runplanner/to/TopActivityTO.java
@@ -0,0 +1,45 @@
+package br.com.runplanner.to;
+
+import java.util.Date;
+
+import br.com.runplanner.util.Utils;
+
+public class TopActivityTO {
+
+	private Double totalDistance;
+	private Double totalTime;
+	private String name;
+	private Long activityId;
+	private Date date;
+	
+	public double getTotalDistance() {
+		return totalDistance;
+	}
+	public void setTotalDistance(double totalDistance) {
+		this.totalDistance = totalDistance;
+	}
+	public String getTotalTime() {
+		return Utils.formataTempo( totalTime.intValue() );
+	}
+	public void setTotalTime(double totalTime) {
+		this.totalTime = totalTime;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getActivityId() {
+		return activityId;
+	}
+	public void setActivityId(Long activityId) {
+		this.activityId = activityId;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}	
+}
